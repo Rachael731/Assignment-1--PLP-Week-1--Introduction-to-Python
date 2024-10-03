13328dbad2b388a19655ff2cd9b3870e230b608c
Rachael731 <rachaeljelagat47@gmail.com>
2024-10-03 16:34:11 +0300

    Add files via upload
    Answers to Assignment 1

---
+++ user_input.py
@@ -0,0 +1,10 @@
+
+# user_input.py
+
+# Asking user for their name, age, and location
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Displaying a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
